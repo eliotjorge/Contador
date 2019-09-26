85e756d9d798ccfabab4b030e00006e6572ddafb
eliotjorge <eliotjorge@hotmail.com>
2019-09-26 13:03:38 +0200

    Add files via upload

---
+++ API.php
@@ -0,0 +1,20 @@
+<?php
+// Read the file contents into a string variable,
+// and parse the string into a data structure
+$str_data = file_get_contents("https://www.sumersol.com/contadorDioxi/dato.json");
+$data = json_decode($str_data,true);
+
+$numero = $data["dato"]["co2"];
+$numeroNue = (float)$numero+3.24;
+//echo $numeroNue;
+ 
+// Modify the value, and write the structure to a file "data_out.json"
+//
+
+$data["dato"]["co2"] = $numeroNue;
+
+$fh = fopen("dato.json", 'w')
+      or die("Error opening output file");
+fwrite($fh, json_encode($data,JSON_UNESCAPED_UNICODE));
+fclose($fh);
+?>
---
+++ script.js
@@ -0,0 +1,37 @@
+ var elemento = document.getElementById("numero");
+	
+	let request = new XMLHttpRequest();
+	request.open('GET', 'https://www.sumersol.com/contadorDioxi/dato.json', true);
+  	request.onload = function () {
+      	let dato = JSON.parse(this.response);
+		var numero = dato["dato"]["co2"];
+		elemento.innerHTML = formatNumber.new(numero);
+		setInterval(function(){
+			numero += 0.027;
+			elemento.innerHTML = formatNumber.new(numero.toFixed(2));
+		},1000);
+    
+    }
+    request.send();
+
+
+var formatNumber = {
+  separador: ".", // separador para los miles
+  sepDecimal: ",", // separador para los decimales
+  formatear: function(num) {
+    num += "";
+    var splitStr = num.split(".");
+    var splitLeft = splitStr[0];
+    var splitRight = splitStr.length > 1 ? this.sepDecimal + splitStr[1] : "";
+    var regx = /(\d+)(\d{3})/;
+    while (regx.test(splitLeft)) {
+      splitLeft = splitLeft.replace(regx, "$1" + this.separador + "$2");
+    }
+    //return this.simbol + splitLeft +splitRight;
+    return splitLeft + splitRight + this.simbol;
+  },
+  new: function(num, simbol) {
+    this.simbol = simbol || "";
+    return this.formatear(num);
+  }
+};
