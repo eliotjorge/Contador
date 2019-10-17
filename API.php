694db5bde616002c40ed28dbf1484d5cb163a90e
eliotjorge <eliotjorge@hotmail.com>
2019-10-17 12:13:05 +0200

    Update API.php

--- API.php
+++ API.php
@@ -1,7 +1,17 @@
 <?php
 // Read the file contents into a string variable,
 // and parse the string into a data structure
-$str_data = file_get_contents("https://www.sumr.com/contadorDioxi/dato.json");
+
+//$str_data = file_get_contents("https://www.sumersol.com/contadorDioxi/dato.json");
+
+$url = "https://www.sumersol.com/contadorDioxi/dato.json";
+$ctx = stream_context_create(array('http' => array('timeout' => 5))); //Con este metodo "stream_context_create()"
+//hacemos que la llamada a la URL se haga a traves de HTTP y no HTTPS que es lo que estaba dando el error "file_get_contents(): SSL: Handshake timed out"
+//tambien le especificamos que haya un timeout de 5s.
+//https://www.experts-exchange.com/questions/26187506/Function-file-get-contents-connection-time-out.html
+$str_data=file_get_contents($url, 0, $ctx);
+
+
 $data = json_decode($str_data,true);
 
 $numero = $data["dato"]["co2"];
@@ -18,3 +28,4 @@ $fh = fopen("dato.json", 'w')
 fwrite($fh, json_encode($data,JSON_UNESCAPED_UNICODE));
 fclose($fh);
 ?>
+
